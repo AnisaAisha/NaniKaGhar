25a5c3a36adf1593714e6a32c90d7b2c5faaa1a8
AnisaAisha <aa04049@st.habib.edu.pk>
2026-02-21 13:31:04 +0500

    Replaced maia room placeholder assets, added diary interaction

---
+++ Assets/Scripts/DiaryInteraction.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class DiaryInteraction : MonoBehaviour
+{
+    [SerializeField] GameObject diaryUIOverlay;
+
+    public bool isDiaryOpened;
+    private int clickCount;
+
+
+    void Awake()
+    {
+        clickCount = 0;
+        isDiaryOpened = false;
+    }
+
+    public void CloseDiary() {
+        diaryUIOverlay.SetActive(false);
+        clickCount++;
+    }
+
+    public void OnMouseDown()
+    {
+        clickCount++;
+    }
+
+    void Update()
+    {
+        // hacky solution because apparently clicking the first time doesn't get triggered
+        if (clickCount == 1) 
+        {
+            diaryUIOverlay.SetActive(true);
+            isDiaryOpened = true;
+        }
+    }
+}
