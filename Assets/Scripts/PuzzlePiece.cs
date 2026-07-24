8f369da42e0bf21d505a17567f999bf4e7c0debe
qzun <qzunairah@hotmail.com>
2026-07-24 16:04:39 +0500

    Added puzzle game mechanic
    I added a very rough-looking, but working mechanic for the puzzle minigame to the courtyard scene in my own folder. It's also there as a separate scene called 'TestPuzzle' in my folder just for testing purposes.

---
+++ Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PuzzlePiece : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
+{
+    private RectTransform rectTransform;
+    private CanvasGroup canvasGroup;
+    private Canvas canvas;
+    public GameObject slot;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponentInParent<Canvas>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        canvasGroup.blocksRaycasts = false;
+        rectTransform.SetAsLastSibling();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        canvasGroup.alpha = 1f; // Reset transparency
+
+        if (Mathf.Abs(rectTransform.anchoredPosition.x - slot.GetComponent<RectTransform>().anchoredPosition.x) <= 70f &&
+        Mathf.Abs(rectTransform.anchoredPosition.y - slot.GetComponent<RectTransform>().anchoredPosition.y) <= 70f)
+        {
+            rectTransform.anchoredPosition = slot.GetComponent<RectTransform>().anchoredPosition;
+        }
+        if (rectTransform.anchoredPosition != slot.GetComponent<RectTransform>().anchoredPosition)
+        {
+            canvasGroup.blocksRaycasts = true; // Restore raycast blocking
+        }
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+         canvasGroup.alpha = 0.7f; // Make item slightly transparent
+    }
+}
